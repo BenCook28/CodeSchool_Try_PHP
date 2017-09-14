ff0c858f5ee73a9ba6f9a5a0ab04e60016434e37
Ben Cook <benthomascook28@gmail.com>
2017-09-14 15:43:05 -0400

    Initial Commit, PHP Basics

---
+++ 0.01 PHP Basics.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+    <head>
+
+    </head>
+    <body>
+        <p><?php 
+        echo 'Hello World';
+        $name = 'Ben';
+        echo $name;
+        ?></p>
+    </body>
+</html>
+
+<!-- PHP files can render HTML as well as PHP -->
+
+<!-- Variables in PHP always start with a $ -->
+<!-- $name = 'Ben' -->
